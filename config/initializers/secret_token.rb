e4c8a57c0f60b5dc6a34554cd0499af330da59f2
Laila Rodriguez <matricharia@gmail.com>
2016-05-15 03:35:43 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoachingBot::Application.config.secret_key_base = '8c5417f76478194efb94dd26384d29f1ed56ab2d977c6a8a866af8e6790b8df9b134845db762c831af9ba3fecd163e06c5146d29701347171ae86456ccdf4bdf'
